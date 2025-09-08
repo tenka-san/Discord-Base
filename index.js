83513c2acb5eaa40de6d178c90bdee2e129239f1
Yuukey <minikrap2@gmail.com>
2025-09-08 11:07:43 +0700

    index.js

--- index.js
+++ index.js
@@ -13,14 +13,20 @@ const {
   SlashCommandBuilder
 } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent
+] });
 
 client.once('ready' () => {
 	console.log(`Sukses Login Sebagai ${readyClient.user.tag}`);
 });
 
 client.on('messageCreate', message => {
-  if (!mesage.content.startWith"/") return;
+  if (!mesage.content.startWith"/") return await message.reply(`Anda Harus Menggunakan Prefix Slash(\'/\') Untuk Memulai`);
+  const args = message.content.slice(1).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase();
 
   if (message.commandName === 'start') {
 	const menu = `Highway Star Bot Are Created By : https://t.me/Yuukeyd7eppeli Here To Help Your Problem\nType \'/allmenu\' To Show All Menu\n</ Highway Star Bot \>`;

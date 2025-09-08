f3ce763d5859fb3bcb34ce2c5df7618f418c972e
Yuukey <minikrap2@gmail.com>
2025-09-08 10:59:44 +0700

    index.js

--- index.js
+++ index.js
@@ -6,20 +6,28 @@ const {
   REST,
   Routes,
   Client,
-  GatewayIntentBits
+  Collection,
+  Events,
+  GatewayIntentBits,
+  MessageFlags,
+  SlashCommandBuilder
 } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, readyClient => {
+client.once('ready' () => {
 	console.log(`Sukses Login Sebagai ${readyClient.user.tag}`);
 });
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isChatInputCommand()) return;
+client.on('messageCreate', message => {
+  if (!mesage.content.startWith"/") return;
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+  if (message.commandName === 'start') {
+	const menu = `Highway Star Bot Are Created By : https://t.me/Yuukeyd7eppeli Here To Help Your Problem\nType \'/allmenu\' To Show All Menu\n</ Highway Star Bot \>`;
+    await message.reply(menu);
+  } else if (message.commandName === 'allmenu') {
+	const menu = `Here Is Your Menu Bruh`;
+    await message.reply(menu);
   }
 });
 
